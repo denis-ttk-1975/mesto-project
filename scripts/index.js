6244886ce23ef22adc178d67fdde4e981be7db41
Denis Ule <denis.ttk@gmail.com>
2021-12-12 21:26:53 +0300

    new card addition feature done

--- scripts/index.js
+++ scripts/index.js
@@ -1,11 +1,26 @@
 const profileEditButton = document.querySelector(".profile__edit-button");
 const profileMemberName = document.querySelector(".profile__member-name");
 const profileMemberOccupation = document.querySelector(".profile__lower-text");
-const popupSaveButton = document.querySelector(".popup__btn-save");
-const popupCloseButton = document.querySelector(".popup__btn-close");
-const popupWindow = document.querySelector(".popup");
-const popupInputForm = document.querySelector(".popup__userdata-input");
-const inputsPopup = popupInputForm.querySelectorAll("input");
+const profileAddPlaceButton = document.querySelector(".profile__add");
+
+const popupProfileInput = document.querySelector(".popup-profile-input");
+const popupSaveButton = popupProfileInput.querySelector(".popup__btn-save");
+const popupCloseProfileInputButton =
+  popupProfileInput.querySelector(".popup__btn-close");
+const popupInputForm = popupProfileInput.querySelector(
+  ".popup__userdata-input"
+);
+const inputsProfilePopup = popupInputForm.querySelectorAll("input");
+
+const popupNewPlaceInput = document.querySelector(".popup-new-place");
+const popupCreateButton = popupNewPlaceInput.querySelector(".popup__btn-save");
+const popupCloseNewPlaceButton =
+  popupNewPlaceInput.querySelector(".popup__btn-close");
+const popupNewPlaceForm = popupNewPlaceInput.querySelector(
+  ".popup__userdata-input"
+);
+const inputsNewPlacePopup = popupNewPlaceForm.querySelectorAll("input");
+
 const elementsArea = document.querySelector(".elements");
 const cardTemplate = document.querySelector("#card-template").content;
 const initialCards = [
@@ -35,23 +50,23 @@ const initialCards = [
   },
 ];
 
-function openPopup() {
-  inputsPopup[0].value = profileMemberName.textContent;
-  inputsPopup[1].value = profileMemberOccupation.textContent;
-  popupWindow.classList.add("popup_opened");
+function openProfilePopup() {
+  inputsProfilePopup[0].value = profileMemberName.textContent;
+  inputsProfilePopup[1].value = profileMemberOccupation.textContent;
+  popupProfileInput.classList.add("popup_opened");
 }
 
-function closePopup() {
-  inputsPopup[0].value = "";
-  inputsPopup[1].value = "";
-  popupWindow.classList.remove("popup_opened");
+function closeProfilePopup() {
+  inputsProfilePopup[0].value = "";
+  inputsProfilePopup[1].value = "";
+  popupProfileInput.classList.remove("popup_opened");
 }
 
-function formSubmitHandler(evt) {
+function formSubmitProfileHandler(evt) {
   evt.preventDefault();
-  profileMemberName.textContent = inputsPopup[0].value;
-  profileMemberOccupation.textContent = inputsPopup[1].value;
-  popupWindow.classList.remove("popup_opened");
+  profileMemberName.textContent = inputsProfilePopup[0].value;
+  profileMemberOccupation.textContent = inputsProfilePopup[1].value;
+  popupProfileInput.classList.remove("popup_opened");
 }
 
 function createElementsArea(array) {
@@ -64,8 +79,36 @@ function createElementsArea(array) {
   });
 }
 
-profileEditButton.addEventListener("click", openPopup);
-popupCloseButton.addEventListener("click", closePopup);
-popupSaveButton.addEventListener("click", formSubmitHandler);
+function openNewPlacePopup() {
+  popupNewPlaceInput.classList.add("popup_opened");
+}
+
+function closeNewPlacePopup() {
+  inputsNewPlacePopup[0].value = "";
+  inputsNewPlacePopup[1].value = "";
+  popupNewPlaceInput.classList.remove("popup_opened");
+}
+
+function formSubmitNewPlaceHandler(evt) {
+  evt.preventDefault();
+  console.log(inputsNewPlacePopup[1]);
+  // alert(inputsNewPlacePopup[0].value);
+  const newElement = cardTemplate.querySelector(".element").cloneNode(true);
+  newElement.querySelector(".element__mask").src = inputsNewPlacePopup[1].value;
+  newElement.querySelector(".element__mask").alt = inputsNewPlacePopup[0].value;
+  newElement.querySelector(".element__card-name").textContent =
+    inputsNewPlacePopup[0].value;
+  console.log(newElement);
+  elementsArea.prepend(newElement);
+  popupNewPlaceInput.classList.remove("popup_opened");
+}
+
+profileEditButton.addEventListener("click", openProfilePopup);
+popupCloseProfileInputButton.addEventListener("click", closeProfilePopup);
+popupSaveButton.addEventListener("click", formSubmitProfileHandler);
+
+profileAddPlaceButton.addEventListener("click", openNewPlacePopup);
+popupCloseNewPlaceButton.addEventListener("click", closeNewPlacePopup);
+popupCreateButton.addEventListener("click", formSubmitNewPlaceHandler);
 
 createElementsArea(initialCards);

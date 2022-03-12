426ffc8784c66fb732323dbb3dadc66a89c3a667
Denis Ule <denis.ttk@gmail.com>
2022-03-13 00:37:32 +0300

    set&delete like on card done

--- src/components/api.js
+++ src/components/api.js
@@ -41,6 +41,33 @@ const configDeleteCard = {
   },
 };
 
+const configSetLike = {
+  method: "PUT",
+  baseUrl: "https://nomoreparties.co/v1/plus-cohort7/cards/likes/",
+  headers: {
+    authorization: "62256703-4f1c-4e78-bfd6-410f01323be3",
+    "Content-Type": "application/json",
+  },
+};
+
+const configDeleteLike = {
+  method: "DELETE",
+  baseUrl: "https://nomoreparties.co/v1/plus-cohort7/cards/likes/",
+  headers: {
+    authorization: "62256703-4f1c-4e78-bfd6-410f01323be3",
+    "Content-Type": "application/json",
+  },
+};
+
+// const configGetCard = {
+//   method: "GET",
+//   baseUrl: "https://nomoreparties.co/v1/plus-cohort7/cards/",
+//   headers: {
+//     authorization: "62256703-4f1c-4e78-bfd6-410f01323be3",
+//     "Content-Type": "application/json",
+//   },
+// };
+
 export const getUserProfile = () => {
   return fetch(configAvatar.baseUrl, { headers: configAvatar.headers }).then(
     (res) => res.json()
@@ -81,3 +108,24 @@ export const deleteCard = (cardId) => {
     headers: configDeleteCard.headers,
   });
 };
+
+export const setLike = (cardId) => {
+  return fetch(configSetLike.baseUrl + cardId, {
+    method: configSetLike.method,
+    headers: configSetLike.headers,
+  });
+};
+
+export const deleteLike = (cardId) => {
+  return fetch(configDeleteLike.baseUrl + cardId, {
+    method: configDeleteLike.method,
+    headers: configDeleteLike.headers,
+  });
+};
+
+// export const getCard = (cardId) => {
+//   return fetch(configGetCard.baseUrl + cardId, {
+//     // method: configGetCard.method,
+//     headers: configGetCard.headers,
+//   });
+// };
--- src/components/card.js
+++ src/components/card.js
@@ -9,47 +9,89 @@ import {
   inputsPictureLinkNewPlacePopup,
 } from "./modal.js";
 
-import { setNewCard, getCardsArray, deleteCard } from "./api.js";
+import {
+  setNewCard,
+  getCardsArray,
+  deleteCard,
+  setLike,
+  deleteLike,
+} from "./api.js";
 
 import { userID } from "./index.js";
 
-function createCard(source, title, rating, ownerID, cardID) {
+function findUserLike(likeData, myUserId) {
+  return likeData.some(function (likerData) {
+    return myUserId === likerData._id;
+  });
+}
+
+function likeButtonHandler(evt) {
+  const cardID = evt.target.closest(".element").dataset.cardId;
+  const likeCounter = evt.target.nextElementSibling;
+
+  if (evt.target.classList.contains("element__like-btn_liked")) {
+    deleteLike(cardID)
+      .then((res) => res.json())
+      .then((res) => {
+        console.log(res);
+        likeCounter.textContent = res.likes.length;
+      });
+  } else {
+    setLike(cardID)
+      .then((res) => res.json())
+      .then((res) => {
+        console.log(res);
+        likeCounter.textContent = res.likes.length;
+      });
+  }
+
+  evt.target.classList.toggle("element__like-btn_liked");
+}
+
+function createCard(source, title, rating, ownerID, cardID, likes) {
   const newCardElement = cardTemplate.querySelector(".element").cloneNode(true);
   const cardImage = newCardElement.querySelector(".element__mask");
   cardImage.src = source;
   cardImage.alt = title;
   newCardElement.querySelector(".element__card-name").textContent = title;
   newCardElement.querySelector(".element__rating").textContent = rating;
-  // console.log(userID);
-  // console.log(ownerID);
+
+  // проверяем наличие лайка на карточке от нашего пользователя
+  if (findUserLike(likes, userID)) {
+    newCardElement
+      .querySelector(".element__like-btn")
+      .classList.add("element__like-btn_liked");
+  }
+
+  // скрываем кнопку удаления карточки если карта создана другим пользователем
   if (userID != ownerID) {
     newCardElement
       .querySelector(".element__delete")
       .classList.add("element__delete_hidden");
   }
+  // устанавливаем пользовательский атрибут ID на карточку из полученного набора данных с сервера
   newCardElement.dataset.cardId = cardID;
+  // далее устанавливаете 3 обработчика(открытие, лайк, удаление)
   cardImage.addEventListener("click", function (evt) {
     openPictureFullView(evt.target);
   });
   newCardElement
     .querySelector(".element__like-btn")
     .addEventListener("click", function (evt) {
-      evt.target.classList.toggle("element__like-btn_liked");
+      likeButtonHandler(evt);
     });
   newCardElement
     .querySelector(".element__delete")
     .addEventListener("click", function (evt) {
       const cardId = evt.target.closest(".element").dataset.cardId;
-      // console.log(cardId);
-      // evt.target.closest(".element").remove();
       deleteCard(cardId).then((res) => {
         if (res.ok) {
           getCardsArray().then((res) => createElementsArea(res));
         }
       });
     });
-  //устанавливаете 3 обработчика(открытие, лайк, удаление)
-  return newCardElement; // возвращаете готовую карточку
+  // возвращаем готовую карточку
+  return newCardElement;
 }
 
 export function addNewPlaceCard(evt) {
@@ -83,7 +125,8 @@ export function createElementsArea(array) {
       item.name,
       item.likes.length,
       item.owner._id,
-      item._id
+      item._id,
+      item.likes
     );
     elementsArea.prepend(newElement);
   });
